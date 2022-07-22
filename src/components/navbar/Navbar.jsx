358fbcdb295ef0e27867474436483287e6c396e2
mfaisal <mfaisal@pnec.nust.edu.pk>
2022-07-22 12:37:07 +0500

    nav bar added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/valab/valab_short_merged.png';
 import './navbar.css';
 
+const Menu = () => (
+    <>
+        <p><a href="#home" >Home</a></p>
+        <p><a href="#whatVALAB" >What is Video Analytics Lab?</a></p>
+        <p><a href="#team" >Team</a></p>
+        <p><a href="#areas" >Research Areas</a></p>
+        <p><a href="#projects" >Projects</a></p>
+        <p><a href="#publication" >Publication</a></p>
+        <p><a href="#contact" >Contact</a></p>
+    </>
+)
+
+
 const Navbar = () => {
+
+    const [toggleMenu, setToggleMenu ]  = useState(false);
+
     return(
         <div className="va__navbar">
             <div className="va__navbar-links">
@@ -11,15 +27,30 @@ const Navbar = () => {
                     <img src={logo} alt="Video Analytics Lab - PNEC" />
                 </div>
                 <div className="va__navbar-links_container" >
-                    <p><a href="#home" >Home</a></p>
-                    <p><a href="#whatVALAB" >What is Video Analytics Lab?</a></p>
-                    <p><a href="#team" >Team</a></p>
-                    <p><a href="#areas" >Research Areas</a></p>
-                    <p><a href="#projects" >Projects</a></p>
-                    <p><a href="#publication" >Publication</a></p>
-                    <p><a href="#contact" >Contact</a></p>
+                    <Menu/>
                 </div>
             </div>
+            {/* <div className="va__navbar-sign">
+                <p>Sign In</p>
+                <button type="button" >Sign Up</button>
+            </div> */}
+
+            <div className="va__navbar-menu">
+                {toggleMenu
+                    ? <RiCloseLine color="#fff" size={27} onClick={()=> setToggleMenu(false)}  />
+                    : <RiMenu3Line color="#fff" size={27} onClick={()=> setToggleMenu(true)}  />
+                }
+
+                {toggleMenu && (
+                    <div className="va__navbar-menu_container scale-up-center">
+                        <div className="va__navbar-menu_container-links">
+                            <Menu/>
+                        </div>
+                    </div>
+                )}
+            </div>
+
+
         </div>
     )
 };
